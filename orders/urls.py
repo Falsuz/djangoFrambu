6efa548e4782121dbddf631994057f349e25062f
Diego Rodriguez <diegordzoro@gmail.com>
2024-05-08 18:24:18 -0600

    login y logout

--- orders/urls.py
+++ orders/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 # url hijo
 urlpatterns = [
-    path('', views.order_list, name='order_list'),
+    path('a', views.order_list, name='order_list'),
     path('home', views.home, name='home'),
     path('order/<int:pk>/', views.order_detail, name='order_detail'),
     path('order/new/', views.order_create, name='order_create'),
--- orders/views.py
+++ orders/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Order
 from .forms import OrderForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+@login_required
 def home(request):
     return render(request, 'home.html', {})
 
